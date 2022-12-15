59d36c44ef79c3d799fa912f9c44b88831615158
Benjamin Ng <34051150+ngben@users.noreply.github.com>
2022-12-16 07:09:20 +1100

    Update cdo shell scripts

--- bxn599/ilamb/DATA/cdo_merge.sh
+++ bxn599/ilamb/DATA/cdo_merge.sh
@@ -15,7 +15,7 @@ module load conda/analysis3
 
 cd $PBS_O_WORKDIR
 
-declare -a StringArray=("ua200" "ua300" "ua500" "ua850" "va200" "va300" "va500" "va850" "ta200" "ta300" "ta500" "ta850" "zg200" "zg300" "zg500" "zg850" "hus600" "hus700" "hus850" "psl" "tas" "omega500" "pr")
+declare -a StringArray=("ua200" "ua300" "ua500" "ua850" "va200" "va300" "va500" "va850" "ta300" "ta500" "ta600" "ta700" "ta850" "zg200" "zg300" "zg500" "zg850" "hus600" "hus700" "hus850" "psl" "tas" "omega500" "pr")
 
 for var in ${StringArray[@]}; do
 
