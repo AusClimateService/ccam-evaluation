d629e8402a027b073e33d40554b0e72eb314edf1
Benjamin Ng <34051150+ngben@users.noreply.github.com>
2023-03-16 17:32:37 +1100

    Added notebook for extremes paper plots

--- bxn599/ilamb/MODELS/link_files.sh
+++ bxn599/ilamb/MODELS/link_files.sh
@@ -16,6 +16,10 @@ ln -s /g/data/ia39/australian-climate-service/test-data/CORDEX-CMIP6/output/AUS-
 ln -s /g/data/xv83/users/bxn599/ACS/icclim_indices/ilamb_barpa/AUS-15/BOM/ECMWF-ERA5/evaluation/none/BOM-BARPA-R/v1/climdex/* /g/data/xv83/users/bxn599/ACS/ilamb/MODELS_all_vars/BARPA_ERA5/.
 ln -s /g/data/ia39/australian-climate-service/test-data/CORDEX-CMIP6/output/AUS-15/BOM/ECMWF-ERA5/evaluation/r1i1p1f1/BOM-BARPA-R/v1/mon/* /g/data/xv83/users/bxn599/ACS/ilamb/MODELS_all_vars/BARPA_ERA5/.
 
+# BARPA NorESM2-MM
+ln -s /g/data/xv83/users/bxn599/ACS/icclim_indices/ilamb_barpa/AUS-15/BOM/NCC-NorESM2-MM/historical/r1i1p1f1/BOM-BARPA-R/v1/climdex/* /g/data/xv83/users/bxn599/ACS/ilamb/MODELS_all_vars/BARPA_NorESM2-MM/.
+ln -s /g/data/ia39/australian-climate-service/test-data/CORDEX-CMIP6/output/AUS-15/BOM/NCC-NorESM2-MM/historical/r1i1p1f1/BOM-BARPA-R/v1/mon/* /g/data/xv83/users/bxn599/ACS/ilamb/MODELS_all_vars/BARPA_NorESM2-MM/.
+
 # CCAM ERA5
 ln -s /g/data/xv83/users/bxn599/ACS/icclim_indices/ilamb_ccam/AUS-10/CSIRO/ECMWF-ERA5/evaluation/none/CSIRO-CCAM-2203/v1/climdex/* /g/data/xv83/users/bxn599/ACS/ilamb/MODELS_all_vars/CCAM_ERA5/.
 ln -s /g/data/xv83/mxt599/ccam_era5_evaluation_aus-10i_12km/drs_cordex/CORDEX/output/AUS-10i/CSIRO/ECMWF-ERA5/evaluation/r1i1p1f1/CSIRO-CCAM-2203/v1/mon/* /g/data/xv83/users/bxn599/ACS/ilamb/MODELS_all_vars/CCAM_ERA5/.
